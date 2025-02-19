c39ad7c1e3bdc978b1a75eaf1de098abfa28c4f1
willyancapone <161588690+WILLYAN12345@users.noreply.github.com>
2025-02-19 11:22:27 -0300

    Add files via upload

---
+++ Atividade1.js
@@ -0,0 +1,3 @@
+let numMes= prompt ("digite seu mês de nascimento")
+let mes= ["Janeiro", "Fevereiro", "Março", "Abril ", "Maio", "Junho","Julho", "Agosto","Setembro", "Outubto", "Novembro", "Dezembro",]
+alert ("Voce nasceu no mês de" + mes [numMes -1])
